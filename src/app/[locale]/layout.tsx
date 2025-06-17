516dacb6542a6e3bbb0089bfac0f3ac4b61ba12f
Vladimir Lebiga <lebiga@Mac.home>
2025-06-17 21:46:24 +0200

    favicon added

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -24,7 +24,7 @@ export default async function LocaleLayout({
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon/favicon.ico" />
       </head>
       <body>
         <Header />
